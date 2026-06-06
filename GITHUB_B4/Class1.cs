a1980d253daa8dd6f8cdb858714a0ea76be98fad
Công Nguyễn <thanhcongspktxd@gmail.com>
2026-06-06 19:46:13 +0700

    Add Hello Command

--- GITHUB_B4/Class1.cs
+++ GITHUB_B4/Class1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,13 @@ namespace GITHUB_B4
 {
     public class B4
     {
-        
+        [CommandMethod("Hello Autocad")]
+        public void SayHello()
+        {
+            // Khai báo Editor cho project
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            //Wrtie Message
+            ed.WriteMessage("Hello AutoCAD");
+        }
     }
 }
